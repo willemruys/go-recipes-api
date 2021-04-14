54597a964ad630344c43f40e63fc395f3440386a
willemruys <wmruys@gmail.com>
2021-04-14 11:31:59 +0200

    unauthorized test

--- main_test.go
+++ main_test.go
@@ -55,15 +55,25 @@ func TestRunMainTests(t *testing.T) {
 	db := models.SetupMockModels()
 	ts := httptest.NewServer(setupServer(db))
 
-	loginRes := CreateUser(db, ts, t)
-	recipe := CreateRecipe(ts, t, loginRes)
-	GetRecipe(ts, t, loginRes, recipe)
-	CreateIncompleteRecipe(ts, t, loginRes)
-	UpdateUserPersonalDetails(ts, t, loginRes)
-	createListResponse := CreateListTest(ts, t, loginRes)
-	GetListAfterCreate(ts, t, loginRes, createListResponse)
-	updateListResponse := UpdateList(ts, t, loginRes, createListResponse)
-	GetListAfterUpdate(ts, t, loginRes, updateListResponse)
+	/* login */
+	loginResUser1 := CreateUser(db, ts, t)
+	loginResUser2 := CreateUserTwo(db, ts, t)
+
+	/* user */
+	UpdateUserPersonalDetails(ts, t, loginResUser1)
+	UpdateUserPersonalDetailsUnauthorized(ts, t, loginResUser1)
+
+	/* recipe */
+	recipe := CreateRecipe(ts, t, loginResUser1)
+	GetRecipe(ts, t, loginResUser1, recipe)
+	CreateIncompleteRecipe(ts, t, loginResUser1)
+
+	/* list */
+	createListResponse := CreateListTest(ts, t, loginResUser1)
+	GetListAfterCreate(ts, t, loginResUser1, createListResponse)
+	updateListResponse := UpdateList(ts, t, loginResUser1, createListResponse)
+	GetListAfterUpdate(ts, t, loginResUser1, updateListResponse)
+	UpdateListUnauthorized(ts, t, loginResUser2, updateListResponse)
 }
 
 func CreateUser(db *gorm.DB, ts *httptest.Server, t *testing.T) LoginResponse {
@@ -123,6 +133,119 @@ func CreateUser(db *gorm.DB, ts *httptest.Server, t *testing.T) LoginResponse {
 
 }
 
+func CreateUserTwo(db *gorm.DB, ts *httptest.Server, t *testing.T) LoginResponse {
+
+	/* Create user */
+	createUser := map[string]string{"Email": "testuser2@gmail.com", "Username": "TestUser2", "Password": "password"}
+
+	jsonCreateUserBody, _ := json.Marshal(createUser)
+
+	req, errCreateUserReq := http.NewRequest("POST", fmt.Sprintf("%s/user", ts.URL), bytes.NewBuffer(jsonCreateUserBody))
+
+	if errCreateUserReq != nil {
+		t.Fatalf("Received error creating user. Error: %s", errCreateUserReq)
+	}
+
+	res, errCreateUser := http.DefaultClient.Do(req)
+
+	if errCreateUser != nil {
+		t.Fatalf("Received error creating user. Error: %s", errCreateUser)
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, received %v", res.StatusCode)
+	}
+	
+	
+	/* Create user */
+	loginUser := map[string]string{"Email": "testuser2@gmail.com", "Password": "password"}
+
+	jsonLoginUser, _ := json.Marshal(loginUser)
+
+	req, errLoginUserReq := http.NewRequest("POST", fmt.Sprintf("%s/login", ts.URL), bytes.NewBuffer(jsonLoginUser))
+
+	if errLoginUserReq != nil {
+		t.Fatalf("Received error creating user. Error: %s", errCreateUserReq)
+	}
+
+	res, errLoginUser := http.DefaultClient.Do(req)
+
+	if errLoginUser != nil {
+		t.Fatalf("Received error creating user. Error: %s", errLoginUser)
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, received %v", res.StatusCode)
+	}
+
+	var decoder = json.NewDecoder(res.Body)
+
+	var loginRes LoginResponse
+
+	if err := decoder.Decode(&loginRes); err != nil {
+		t.Fatalf("Error decoding json")
+	}
+
+	return loginRes
+}
+
+
+func UpdateUserPersonalDetails(ts *httptest.Server, t *testing.T, loginRes LoginResponse) {
+
+	updateUserDetails := map[string]string{"Email": "updatedUser@gmail.com", "Username": "updatedUsername"}
+
+	jsonUpdateUserDetails, _ := json.Marshal(updateUserDetails)
+	userId, err := auth.ExtractTokenIDFromToken(loginRes.Token)
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	req, err := http.NewRequest("PATCH", fmt.Sprintf("%s/user/personal-details/%v", ts.URL, userId), bytes.NewBuffer(jsonUpdateUserDetails))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", loginRes.Token))
+
+	completeResp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+        t.Fatalf("Expected no error, got %v", err.Error())
+    }
+
+	if completeResp.StatusCode != 200 {
+        t.Fatalf("Expected status code 200, got %v", completeResp.StatusCode)
+    }
+}
+
+func UpdateUserPersonalDetailsUnauthorized(ts *httptest.Server, t *testing.T, loginRes LoginResponse) {
+
+	updateUserDetails := map[string]string{"Email": "updatedUserUnauth@gmail.com", "Username": "updatedUserUnauth"}
+
+	jsonUpdateUserDetails, _ := json.Marshal(updateUserDetails)
+
+	req, err := http.NewRequest("PATCH", fmt.Sprintf("%s/user/personal-details/%v", ts.URL, 9999), bytes.NewBuffer(jsonUpdateUserDetails))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", loginRes.Token))
+
+	completeResp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+        t.Fatalf("Expected no error, got %v", err.Error())
+    }
+
+	if completeResp.StatusCode != 401 {
+        t.Fatalf("Expected status code 401, got %v", completeResp.StatusCode)
+    }
+}
+
+/* recipes test */
 func CreateRecipe(ts *httptest.Server, t *testing.T, loginRes LoginResponse) ResponseCreateRecipe {
 
 	createRecipeValuesComplete := map[string]string{"Title": "my test recipe", "Ingredients": "my test ingredients"}
@@ -208,38 +331,7 @@ func CreateIncompleteRecipe(ts *httptest.Server, t *testing.T, loginRes LoginRes
 }
 
 
-func UpdateUserPersonalDetails(ts *httptest.Server, t *testing.T, loginRes LoginResponse) {
-
-	updateUserDetails := map[string]string{"Email": "updatedUser@gmail.com", "Username": "updatedUsername"}
-
-	jsonUpdateUserDetails, _ := json.Marshal(updateUserDetails)
-	userId, err := auth.ExtractTokenIDFromToken(loginRes.Token)
-
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
-
-	req, err := http.NewRequest("PATCH", fmt.Sprintf("%s/user/personal-details/%v", ts.URL, userId), bytes.NewBuffer(jsonUpdateUserDetails))
-
-	if err != nil {
-		t.Fatalf("Expected no error, got %v", err)
-	}
-
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", loginRes.Token))
-
-	completeResp, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-        t.Fatalf("Expected no error, got %v", err.Error())
-    }
-
-	if completeResp.StatusCode != 200 {
-        t.Fatalf("Expected status code 200, got %v", completeResp.StatusCode)
-    }
-}
-
-
-/* Recipes test */
+/* List test */
 func CreateListTest(ts *httptest.Server, t *testing.T, loginRes LoginResponse) ResponseCreateList {
 
 	createListInput := map[string]string{"Title": "my test list", "Description": "My test description"}
@@ -390,5 +482,31 @@ func GetListAfterUpdate(ts *httptest.Server, t *testing.T, loginRes LoginRespons
 	if getListResponse.List.Description != listResponse.List.Description {
 		t.Fatalf("Expected matching list descriptions")
 	}
+
+}
+
+func UpdateListUnauthorized(ts *httptest.Server, t *testing.T, loginRes LoginResponse, listResponse UpdateListResponse) {
+
+	createListInput := map[string]string{"Title": "my test list adjusted", "Description": "My test description adjusted"}
+
+	jsonCreateListInput, _ := json.Marshal(createListInput)
+
+	req, err := http.NewRequest("PATCH", fmt.Sprintf("%s/list/%v", ts.URL, listResponse.List.ID), bytes.NewBuffer(jsonCreateListInput))
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", loginRes.Token))
+
+	completeResp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+        t.Fatalf("Expected no error, got %v", err.Error())
+    }
+
+	if completeResp.StatusCode != 401 {
+        t.Fatalf("Expected status code 401, got %v", completeResp.StatusCode)
+    }
 
 }
